5d5968499558a957a51a6f9ad8388ea130e42e7a
Marcus <nguyenhoang1151995@gmail.com>
2025-07-20 22:23:19 +0700

    lession 1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://pw-blog.congcu.org/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("Playwright Practice Blog – Học automation test từ chưa biết gì – Playwright Practice Blog – Học automation test từ chưa biết gì");
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://pw-blog.congcu.org');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Khóa học automation test cho người chưa biết gì' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Thông tin khoá học' })).toBeVisible();
 });
